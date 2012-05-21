84bbd920c436e3d509fbbe7a987ca347d870946f
afoncubierta <afoncubierta@106-14.info2>
2012-05-21 13:00:15 +0200

    Primer commit

---
+++ src/clase1.java
@@ -0,0 +1,9 @@
+
+public class clase1 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Syo la clase 1");
+	}
+
+}
---
+++ src/clase2.java
@@ -0,0 +1,10 @@
+
+public class clase2 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Soy la clase 2");
+
+	}
+
+}
