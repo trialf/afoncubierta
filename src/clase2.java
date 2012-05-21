52be82ed1325f8a0991c69017c580617fc7d27ea
afoncubierta <afoncubierta@106-14.info2>
2012-05-21 13:13:13 +0200

    Creado metodo_rama_master_clase2

--- src/clase2.java
+++ src/clase2.java
@@ -4,7 +4,9 @@ public class clase2 {
 	public void metodo1_clase2(){
 		
 	}
-	
+	public void metodo_rama_master_clase2(){
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Soy la clase 2");
 
